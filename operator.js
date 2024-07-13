44bb1d43bd1446b6b7554b67a4010ec4adec5832
Sambridhi Shrestha <shresthasambridi771@gmail.com>
2024-07-13 14:09:54 +0545

    operator

---
+++ operator.js
@@ -0,0 +1,75 @@
+//Arthimatic operator
+// let a=5;
+// let b=2;
+// console.log("a=",a,"& b =",b);  //=
+// console.log("a+b =",a+b);       //+
+// console.log("a-b =",a-b);       //-
+// console.log("a*b =",a*b);       //*
+// console.log("a/b =",a/b);      // /
+// console.log("a%b =",a%b);      // % Modulus
+// console.log("a**b=",a**b);     //5^2 exponential
+
+
+
+//Unary opeerator
+// let a =5;
+// let b=2;
+// console.log("a=",a,"b=",b);
+// // a++;                       // post increment 
+
+// // a--;                          // post decrement
+// // console.log(a);
+// console.log("++a=",++a);        //pre increment 
+// console.log("--a=",--b);        //pre decrement 
+
+
+
+//Asssignment Operator
+//  let a=5;
+//  let b=6;
+
+//  a += 4; //a=a+4
+//  console.log("a=",a);
+// a -= 4;
+// console.log("a=",a);
+// a*=4;
+// console.log("a=",a);
+// a%=4;
+// console.log("a=",a);
+// a**=4;
+// console.log("a=",a);
+
+
+//comparison operator
+//  let a=5;
+//  let b=3;
+//  console.log("a==b",a==b); //false
+//  console.log("a!=b",a!=b); //true
+//  console.log("a>b",a>b); //true
+//  console.log("a<b",a<b); //false
+//  console.log("a>=b",a>=b); //true
+//  console.log("a<=b",a<=b); //false
+
+
+ //logical operator 
+//  let a=5;
+// let b=2;
+
+// let cond1= a>b; //true  //in && if bpoth true then only true , if only one false then it becomes false
+// let cond2=a===6;//true
+// console.log("cond1 && cond2 =" , cond1 && cond2);  //&&
+
+
+//in || if one also true then true , it does not need to be both true 
+// console.log("cond1 || cond2 =". a<b || b===2); //||
+
+//logical not =oppposite
+// console.log("!(6<5)=",!(a<b));    //!
+
+
+
+
+
+
+
+
