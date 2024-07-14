61767c97dc7fe5616ace975227d4de18a57deba8
Sambridhi Shrestha <shresthasambridi771@gmail.com>
2024-07-14 14:44:50 +0545

    string

--- string.js
+++ string.js
@@ -9,9 +9,62 @@
 // let specialString='This is a template literal';
 // console.log( typeof specialString);
 
-let obj={
-    item:"personalbar",
-    price:10,
-};
-let output=`The cost of ${obj.item} is ${obj.price} rupees`;//use of template literal
-console.log(output);
+// let obj={
+//     item:"personalbar",
+//     price:10,
+// };
+// let output=`The cost of ${obj.item} is ${obj.price} rupees`;//use of template literal
+// console.log(output);
+
+// console.log("Sam \nsth"); 
+
+
+//------------------------------------------------------------------
+//string Method in js
+
+
+// 1 .str.toUppercase()
+//str.toLowerCase()
+//str.trim()//remove the space from the string
+
+//let str="Thankot";
+// let newStr=str.toUpperCase();
+// console.log(str);
+// console.log(newStr);
+
+//  2.  let str="APSARA";
+// let newStr=str.toLowerCase();
+// console.log(str);
+// console.log(newStr);
+
+//  3 . let str ="    java    script";
+// console.log(str.trim());
+
+// 4. str.slice(start,end)
+// let str="0123456";
+// console.log(str.slice(1,3));
+
+//5. str.concat(start,end)  ..concat
+// let str1="give";
+// let str2="me";
+// let result=(str1.concat(str2));  or result=str1+str2
+// console.log(result);
+
+//6 . str.replace(searchVal.newVal)
+// let str="hello";
+// console.log(str.replace("h","y"));
+
+
+
+//7.str.charat(idx)
+// let str =" ilovejavascript";
+// console.log(str.charAt(2));
+
+
+//Lets practise 
+// Prompt the user to enter their full name. Generaate a username for them based on the Input. Start usrname with @ , followed by their full name and ending with the fullname length.
+
+// let fullName =prompt("Enter your fullname without spaces");
+// let username ="@"+fullname+fullname.length;
+// console.log(fullName);
+
